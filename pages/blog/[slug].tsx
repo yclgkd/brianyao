8040dc15943ba4322531cb07f7b3a5d2c99abca6
brian <chunlai0928@gmail.com>
2022-10-18 23:21:27 +0800

    feat: code highlight

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -3,12 +3,10 @@ import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import SyntaxHighlighter from 'react-syntax-highlighter'
 import { Header, Footer } from '../../components'
 import Head from 'next/head'
 import * as Config from '@/config'
-
-const components = { SyntaxHighlighter }
+import rehypeHighlight from 'rehype-highlight'
 
 type PostPageProps = {
   frontMatter: {
@@ -35,12 +33,17 @@ const PostPage = ({ frontMatter: { title, description, date }, mdxSource }: Post
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={Config.BLOG_AUTHOR_AVATAR} />
+        <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
+        />
       </Head>
       <Header />
       <div className="py-3">
         <h1 className="mb-2 text-5xl">{title}</h1>
         <article className="prose dark:prose-invert">
-          <MDXRemote {...mdxSource} components={components} />
+          <MDXRemote {...mdxSource} />
         </article>
       </div>
       <Footer />
@@ -72,7 +75,19 @@ const getStaticProps = async ({ params: { slug } }: GetStaticProps) => {
   const markdownWithMeta = fs.readFileSync(path.join('_posts', slug + '.mdx'), 'utf-8')
 
   const { data: frontMatter, content } = matter(markdownWithMeta)
-  const mdxSource = await serialize(content)
+  const mdxSource = await serialize(content, {
+    // made available to the arguments of any custom mdx component
+    scope: {},
+    // MDX's available options, see the MDX docs for more info.
+    // https://mdxjs.com/packages/mdx/#compilefile-options
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [rehypeHighlight],
+      format: 'mdx'
+    },
+    // Indicates whether or not to parse the frontmatter from the mdx source
+    parseFrontmatter: false
+  })
 
   return {
     props: {
