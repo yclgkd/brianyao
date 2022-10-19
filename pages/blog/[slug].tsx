86a8c92719ad4403594bdff77f6d58329b5ec451
brian <chunlai0928@gmail.com>
2022-10-19 21:27:52 +0800

    chore: google structured data

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -7,6 +7,7 @@ import { Header, Footer } from '../../components'
 import Head from 'next/head'
 import * as Config from '@/config'
 import rehypeHighlight from 'rehype-highlight'
+import Script from 'next/script'
 
 type PostPageProps = {
   frontMatter: {
@@ -18,16 +19,29 @@ type PostPageProps = {
   mdxSource: MDXRemoteSerializeResult<Record<string, unknown>>
 }
 
-const PostPage = ({ frontMatter: { title, description, date }, mdxSource }: PostPageProps) => {
+const PostPage = ({
+  frontMatter: { title, description, date, tags },
+  mdxSource
+}: PostPageProps) => {
+  const publishDate = new Date(date).toISOString()
   return (
     <div className="blog-container">
       <Head>
         <title>{title}</title>
+        <meta
+          name="keywords"
+          content={
+            tags.length
+              ? tags.join(', ')
+              : 'Brian Yao, Brian, Blog, 个人网站, Web 前端, JavaScript, CSS, HTML'
+          }
+        />
         <meta name="description" content={description} />
         <meta property="og:site_name" content={Config.BLOG_TITLE} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={Config.BLOG_AUTHOR_AVATAR} />
+        <meta property="article:published_time" content={publishDate} />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content={Config.BLOG_AUTHOR_TWITTER_USERNAME} />
         <meta name="twitter:title" content={title} />
@@ -47,19 +61,40 @@ const PostPage = ({ frontMatter: { title, description, date }, mdxSource }: Post
         </article>
       </div>
       <Footer />
+      <Script
+        id="structured-data"
+        strategy="afterInteractive"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `
+
+            {
+              "@context": "https://schema.org",
+              "@type": "BlogPosting",
+              "headline": "${title}",
+              "image": "${Config.BLOG_AUTHOR_AVATAR}",
+              "datePublished": "${publishDate}",
+              "dateModified": "${publishDate}",
+              "author": {
+                "@type": "Person",
+                "name": "${Config.BLOG_AUTHOR}"
+              },
+              "description": "${description}"
+            }
+            `
+        }}
+      />
     </div>
   )
 }
 
 const getStaticPaths = async () => {
   const files = fs.readdirSync(path.join('_posts'))
-
   const paths = files.map((filename) => ({
     params: {
       slug: filename.replace('.mdx', '')
     }
   }))
-
   return {
     paths,
     fallback: false
@@ -71,9 +106,9 @@ type GetStaticProps = {
     slug: string
   }
 }
+
 const getStaticProps = async ({ params: { slug } }: GetStaticProps) => {
   const markdownWithMeta = fs.readFileSync(path.join('_posts', slug + '.mdx'), 'utf-8')
-
   const { data: frontMatter, content } = matter(markdownWithMeta)
   const mdxSource = await serialize(content, {
     // made available to the arguments of any custom mdx component
@@ -88,7 +123,6 @@ const getStaticProps = async ({ params: { slug } }: GetStaticProps) => {
     // Indicates whether or not to parse the frontmatter from the mdx source
     parseFrontmatter: false
   })
-
   return {
     props: {
       frontMatter,
